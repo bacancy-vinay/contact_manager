3029b4c2d50f3fc8e4726afb25d5d9f7b60be6e6
bacancy-vinay <vinay.sharma@bacancytechnology.com>
2020-05-04 18:23:28 +0530

    validation-added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,29 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.all.page(params[:page])
+    if params[:group_id]&& !params[:group_id].empty?
+      @contacts = Contact.where(group_id: params[:group_id]).order(created_at: :desc).page(params[:page])
+    else
+      @contacts = Contact.order(created_at: :desc).page(params[:page])
+    end
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Contact was created"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
   end
 end
